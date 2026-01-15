f06f61b3b5818424c6019f9690401a10d500f8f7
29delgaizo <29delgaizo@da.org>
2026-01-15 13:49:04 -0500

    updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+# FIRST PYGAME LAB/SQUARE CHASING GAME
+__AUTHOR__ = "JACKSON_DEL_GAIZO"
+__VERSION__ = "1-15-2026"
+
+
+
 import random
 
 
 import pygame
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # symbolic constants
+def main():
     BLACK = (0, 0, 0)
     BLUE = (0, 0, 255)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     # Before the loop: create speed variables
-    speed_x = random.randint(-2,2)
+    speed_x = random.randint(-2, 2)
     if speed_x == 0:
         speed_x = 1
-    speed_y = random.randint(-2,2)
+    speed_y = random.randint(-2, 2)
     if speed_y == 0:
         speed_y = 1
     size = (1000, 800)
@@ -29,8 +33,8 @@ if __name__ == '__main__':
     # keep the animation loop going
     running = True
     # this is a Rect object in Pygame -- draw to the screen
-    rect = pygame.Rect(random.randint(0, size[0]-100), random.randint(0, size[1]-100), 100, 100)
-    playerRect = pygame.Rect((size[0]-10)/2, (size[1]-10)/2, 20, 20)
+    rect = pygame.Rect(random.randint(0, size[0] - 100), random.randint(0, size[1] - 100), 100, 100)
+    playerRect = pygame.Rect((size[0] - 10) / 2, (size[1] - 10) / 2, 20, 20)
 
     while running:
         for event in pygame.event.get():
@@ -39,16 +43,16 @@ if __name__ == '__main__':
         # this code processes key movement with the small rect object
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            playerRect.move_ip(0,-1)
+            playerRect.move_ip(0, -1)
         if keys[pygame.K_s]:
-            playerRect.move_ip(0,1)
+            playerRect.move_ip(0, 1)
         if keys[pygame.K_a]:
-            playerRect.move_ip(-1,0)
+            playerRect.move_ip(-1, 0)
         if keys[pygame.K_d]:
-            playerRect.move_ip(1,0)
+            playerRect.move_ip(1, 0)
         playerRect.clamp_ip(screen.get_rect())
-        #0-255 2^8 = RGB 0000 0000 1111 1111
-        #if
+        # 0-255 2^8 = RGB 0000 0000 1111 1111
+        # if
         screen.fill(BLACK)
         pygame.draw.rect(screen, BLUE, rect)
         pygame.draw.rect(screen, GREEN, playerRect)
@@ -65,9 +69,9 @@ if __name__ == '__main__':
         if playerRect.colliderect(rect):
             running = False
     running = True
-    font = pygame.font.SysFont("Bold", 20) #20=size
+    font = pygame.font.SysFont("Bold", 20)  # 20=size
     text_surface = font.render("Gameover, click to quit", True, RED)
-    text_rect = text_surface.get_rect(centerx=size[0]/2, centery=size[1]/2)
+    text_rect = text_surface.get_rect(centerx=size[0] / 2, centery=size[1] / 2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -78,6 +82,12 @@ if __name__ == '__main__':
         screen.blit(text_surface, text_rect)
         pygame.display.flip()
     pygame.quit()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+    # symbolic constants
+
 
 
 
